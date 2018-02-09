8c487be5fd6ed9b4402732aa0973699414b4a619
gaelphilippe <gael.philippe@skills4.io>
2018-02-09 15:06:18 +0100

    micro test commit

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const authenticate = (token) => (authorization) => {
 }
 
 const execute = (botmatic, client, req, res, type) => {
-  debug(`Executing ${type}...`)
+  debug(`Executing ${type}…`)
 
   let elementFound = null;
   var regex;
